68c7cf11b6ebb1bc98b8147d3e7df94b38f2da17
Yo <jaha_devcrack@gmail.com>
2019-03-21 15:22:55 -0600

    soft sphere process complete

--- main/machine/process_lite.py
+++ main/machine/process_lite.py
@@ -31,8 +31,7 @@ def exe_hard_sphere(frac_vol):
     if out:
         print('OK', out)
     if error:
-        print('Error', error.strip())
-        return ':('
+        print('Error', error.strip())        
     if not pcs.poll():
         print("Program hard_sphere execute finish")
 
@@ -56,6 +55,14 @@ def exe_soft_sphere(fv, it):
     exe_p = os.getcwd()
     pcs = sbp.Popen([exe_p + '/02SSphere', str(fv), str(it)], stdout=sbp.PIPE, stderr=sbp.PIPE)       
     out, error = pcs.communicate()
+    if out:
+        print('OK', out)
+    if error:
+        print('Error', error.strip())        
+    if not pcs.poll():
+        print("Program soft_sphere execute finish")
+
+
 
 if __name__ == "__main__":
-    exe_hard_sphere(0.5)
+    exe_soft_sphere(0.5, 0.1)
