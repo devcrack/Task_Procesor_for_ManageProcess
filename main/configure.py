2cbb62e20f1296a0c886d18a2888a0a6b6373dc9
Yo <jaha_devcrack@gmail.com>
2019-03-21 14:00:00 -0600

    Update the rabbit connection

--- main/configure.py
+++ main/configure.py
@@ -8,10 +8,10 @@ class BaseConfig:
     SECRET_KEY = os.urandom(24)
     SESSION_COOKIE_SECURE = True
     #Celery Configure     
-    CELERY_BROKER_URL =  'amqp://guest@localhost//'
+    #CELERY_BROKER_URL =  'amqp://guest@localhost//' #This broker is for localhost
+    CELERY_BROKER_URL = 'amqp://devcrack:mientras123@192.168.56.101:5672//'
     CELERY_RESULT_BACKEND = 'mongodb://user:mientras123@ds157574.mlab.com:57574/connect_to_mongo'
-    #CELERY_INCLUDE = ['task_s.mail_tasks']   
-    CELERY_INCLUDE = ['main.task_s.task1']   
+    #CELERY_INCLUDE = ['main.task_s.task1']# Referenc to celery task files.
 
 
 class Development_Config(BaseConfig):
